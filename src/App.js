378eb432067934e5dfb45fe94495c1af65783213
Badr <baderhelal567@gmail.com>
2023-02-13 11:15:56 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import QrReader from 'modern-react-qr-reader'
 
-class App extends Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [result, setResult] = React.useState('No Result')
 
-    this.state = {
-      result: 'No result'
-    }
-
-    this.handleError = this.handleError.bind(this);
-    this.handleScan = this.handleScan.bind(this);
-  }
-
-  handleScan = data => {
+  const handleScan = data => {
     if (data) {
-      this.state.result = data;
-      console.log(this.state.result);
-      this.setState({ result: data });
+      setResult({ result: data });
+      console.log(result);
       alert('')
     }
   }
 
-  handleError = err => {
+  const handleError = err => {
     console.error(err)
   }
 
-  render() {
-    return (
-      <div style={{
-        position: 'relative',
-        left: "40% ",
-        
-      }}>
-        <QrReader
-          delay={300}
-          facingMode={"environment"}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          style={{ width: '20%',  }}
-         />
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
+  return (
+    <div style={{
+      position: 'relative',
+      left: "40% ",
+
+    }}>
+      <QrReader
+        delay={300}
+        facingMode={"environment"}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '20%', }}
+      />
+      <p>{result}</p>
+    </div>
+  )
 }
 
 export default App;
